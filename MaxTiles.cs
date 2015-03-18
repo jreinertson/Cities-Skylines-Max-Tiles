170d8463d494c537686700d48df49ad19ac81131
jreinertson <j.reinertson@hotmail.com>
2015-03-18 16:34:33 -0400

    Allows 25 tiles to be purchased in Cities: Skylines
    Included is all my working files, if you just want the mod place
    MaxTiles.dll in Cities Skylines/Files/Mods/MaxTiles

---
+++ MaxTiles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ICities;
+using UnityEngine;
+
+namespace MaxTilesMod
+{
+    public class Maxtiles : IUserMod {
+
+        public string Name {
+            get { return "Max Tiles"; }
+        }
+
+        public string Description {
+            get { return "Can buy more than 9 tiles of a city"; }
+        }
+    }
+
+    public class MaxTilesAreas : AreasExtensionBase{
+		 
+        public override bool OnCanUnlockArea(int x, int z, bool originalResult){
+			return true; 
+		 }
+        public override void OnCreated(IAreas areas)
+        {
+            base.OnCreated(areas);
+            areas.maxAreaCount = 25;
+        }
+         
+
+     }
+} 
